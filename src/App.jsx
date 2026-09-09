85d6af06ce3bd4f69266ae8104422fba3c8877ae
Minhazul Islam Rikat <minhazulrikat@gmail.com>
2026-09-09 21:34:25 +0600

    app jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Players from "./Sections/Players/Players";
 
 function App() {
   const [totalCoin, setTotalCoin] = useState(50000);
-
+// checking  
   return (
     <>
       <Navbar totalCoin={totalCoin}></Navbar>
