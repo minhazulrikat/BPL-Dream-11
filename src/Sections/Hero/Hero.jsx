7d19e96b91acee3fce242304822dcfb26529b9d9
Minhazul Islam Rikat <minhazulrikat@gmail.com>
2026-09-09 22:18:49 +0600

    small css formating change

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -3,7 +3,7 @@
 const Hero = () => {
     return (
      <div className="hero bg-base-200 min-h-[60vh]">
-  <div className="hero-content text-center">
+  <div className="hero-content text-center ">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">Hello there</h1>
       <p className="py-6">
